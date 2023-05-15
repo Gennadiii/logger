c26490594778beaea4d7cf6318f7aeec175c0bbe
Gennadii Mishchevskii <g.mishchevskii@gmail.com>
2023-05-15 10:23:28 +0300

    test [skip ci]

---
+++ mocha.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  diff: true,
+  reporter: "spec",
+  slow: "75",
+  timeout: "100",
+  ui: "bdd",
+  spec: ['dist/specs/**/*.spec.js'],
+  color: true,
+};
---
+++ specs/some.spec.ts
@@ -0,0 +1,7 @@
+import {expect} from "chai";
+
+describe('Some spec', () => {
+  it('some test', () => {
+    expect(true).to.be.true;
+  })
+})
