668dc93a85cd82e33178cd08876624a25e007c31
Gennadii Mishchevskii <g.mishchevskii@gmail.com>
2023-05-16 17:25:04 +0300

    example_10_pageAsyncFn

---
+++ examples/example_10_pageAsyncFn/components.ts
@@ -0,0 +1,36 @@
+import {classLogDecorator} from "./logger_DIFF";
+
+export abstract class BaseComponent {
+  protected constructor(public locator: string) {}
+
+  async click(): Promise<void> {
+    await this.sleep(200);
+  }
+
+  async isDisplayed(): Promise<boolean> {
+    await this.sleep(300);
+    return true;
+  }
+
+  async sleep(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+}
+
+@classLogDecorator
+export class InputField extends BaseComponent {
+  constructor(public override locator: string) {
+    super(locator);
+  }
+
+  async enterText(text: string): Promise<void> {
+    await this.sleep(300);
+  }
+}
+
+@classLogDecorator
+export class Button extends BaseComponent {
+  constructor(public override locator: string) {
+    super(locator);
+  }
+}
---
+++ examples/example_10_pageAsyncFn/services.ts
@@ -0,0 +1,22 @@
+import {LandingPo, LoginPo} from "./pages";
+
+export class SomeVeryLongNameForLoginService {
+  constructor(public loginPage: LoginPo, private landingPage: LandingPo) {}
+
+  async using(params: usingInterface): Promise<void> {
+    const {email, password} = params;
+    await this.loginPage.getModeButton("admin").click();
+    await this.loginPage.getInputs("admin").emailInputField.enterText(email);
+    await this.loginPage
+      .getInputs("admin")
+      .passwordInputField.enterText(password);
+    await this.loginPage.signInButton.click();
+    await this.landingPage.verifyIsOpen();
+  }
+}
+
+interface usingInterface {
+  email: string;
+  password: string;
+}
+
