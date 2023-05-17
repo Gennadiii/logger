ba81fb99f8b743eeead065d361834dbf49f7d028
Gennadii Mishchevskii <g.mishchevskii@gmail.com>
2023-05-17 12:46:00 +0300

    example_13_passwords

---
+++ examples/example_13_passwords/assembler.ts
@@ -0,0 +1,25 @@
+import {pageLogProxyDecorator, serviceProxyDecorator} from "./logger_DIFF";
+import {LandingPo, LoginPo} from "./pages";
+import {SomeVeryLongNameForLoginService} from "./services";
+
+export const service = {
+  login: serviceFactory({
+    Service: SomeVeryLongNameForLoginService,
+    Pages: [LoginPo, LandingPo],
+  }) as SomeVeryLongNameForLoginService,
+};
+
+function serviceFactory(params: serviceFactoryInterface) {
+  const {Pages, Service} = params;
+  const pageInstances = Pages.map(Page => pageLogProxyDecorator(new Page()));
+  return serviceProxyDecorator(new Service(...pageInstances));
+}
+
+interface serviceFactoryInterface {
+  Service: Constructable;
+  Pages: Constructable[];
+}
+
+interface Constructable {
+  new (...args: any[]): any;
+}
---
+++ examples/example_13_passwords/pages.ts
@@ -0,0 +1,59 @@
+import {BaseComponent, Button, InputField} from "./components";
+
+abstract class BasePo {
+  abstract staticElement: BaseComponent;
+
+  async isOpen(): Promise<boolean> {
+    return this.staticElement.isDisplayed();
+  }
+
+  async verifyIsOpen(): Promise<void> {
+    if (!(await this.isOpen())) {
+      throw new Error(`${this.constructor.name} is not opened`);
+    }
+  }
+
+  async sleep(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+}
+
+export class LoginPo extends BasePo {
+  signInButton = new Button("signInButtonLocator");
+
+  constructor() {
+    super();
+  }
+
+  get staticElement() {
+    return this.signInButton;
+  }
+
+  getModeButton(mode: string): Button {
+    return new Button(`modeLocator_${mode}`);
+  }
+
+  getInputs(mode: string): inputInterface {
+    return {
+      emailInputField: new InputField(`emailLocator_${mode}`),
+      passwordInputField: new InputField(`passwordLocator_${mode}`),
+    };
+  }
+}
+
+export class LandingPo extends BasePo {
+  userProfileButton = new Button("userProfileButtonLocator");
+
+  constructor() {
+    super();
+  }
+
+  get staticElement() {
+    return this.userProfileButton;
+  }
+}
+
+interface inputInterface {
+  emailInputField: InputField;
+  passwordInputField: InputField;
+}
